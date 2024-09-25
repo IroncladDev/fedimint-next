7d225d0bf6ca59f0b5a289b0333880bc7a379c75
Conner Ow <conner@connerow.dev>
2024-09-25 17:51:43 -0500

    fix up stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Container from "./components/container";
-
 export default function Home() {
   return (
-    <Container>
       <div className="flex flex-col gap-2">
         <h1 className="text-2xl font-bold">Fedimint Next.js Examples</h1>
       </div>
-    </Container>
   );
 }
