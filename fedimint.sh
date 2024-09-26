d76f1fcf0ec1c115604f2db685382feed9118ea2
Conner Ow <conner@connerow.dev>
2024-09-26 14:28:29 -0500

    add to script

--- fedimint.sh
+++ fedimint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Check if the directory exists
+if [ ! -d "./fm_db/fm.db" ]; then
+    # Create the directory
+    mkdir -p "./fm_db/fm.db"
+    echo "Directory created."
+else
+    echo "Directory already exists."
+fi
+
 if ! command -v fedimint-clientd >/dev/null 2>&1; then
     echo "fedimint-clientd not installed. Installing..."
     cargo install fedimint-clientd
