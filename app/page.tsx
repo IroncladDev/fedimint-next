ab2ced9a35bb980eb6f333d6d6a1ed6597f80a55
Conner Ow <conner@connerow.dev>
2024-09-25 19:04:08 -0500

    format

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-      <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold">Fedimint Next.js Examples</h1>
-      </div>
-  );
+    return (
+        <div className="flex flex-col gap-2">
+            <h1 className="text-2xl font-bold">Fedimint Next.js Examples</h1>
+        </div>
+    );
 }
