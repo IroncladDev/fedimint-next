6bf840f341488e0fc9e14585ef221d3d531eb819
Conner Ow <conner@connerow.dev>
2024-09-30 15:07:11 -0500

    fix: cors

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,23 @@ const nextConfig = {
 
         return config;
     },
+    headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,HEAD,PUT,PATCH,POST,DELETE",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
